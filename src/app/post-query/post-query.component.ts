ac0f86fd831fa14df5125b3ee2155d3828bdaf2a
ankush.dabra@primussoft.com-at-161051381867 <ankush.dabra@primussoft.com>
2018-06-01 22:01:35 +0530

    takingupdate

--- src/app/post-query/post-query.component.ts
+++ src/app/post-query/post-query.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostQueryModel } from './post-query.model';
 
 @Component({
   selector: 'app-post-query',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostQueryComponent implements OnInit {
   selectedCategory = 'c1';
+  title: string ;
+  queryContent: string;
+  queryArray: Array<PostQueryModel> = [new PostQueryModel('1', 'One'), new PostQueryModel('2', 'Two')];
 
   constructor() { }
 
---
+++ src/app/post-query/post-query.model.ts
@@ -0,0 +1,10 @@
+
+export class PostQueryModel {
+  title: String;
+  queryDescription: String;
+
+  constructor(title: String, queryDescription: String) {
+    this.title = title;
+    this.queryDescription = queryDescription;
+  }
+}
